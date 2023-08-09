6d36afcf3645ca1df20a27036c9da5ed39beae79
Fedor Sokolov <sokolov.fedor.g@gmail.com>
2023-08-09 15:24:41 +0400

    Create button component

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,29 @@
+import './button.styles.scss'
+import { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+  buttonType?: "google" | "inverted",
+  type?: 'submit' | 'reset' | 'button';
+  disabled?: boolean,
+
+}
+
+const BUTTON_TYPE_CLASSES = {
+  google: 'google-sign-in',
+  inverted: 'inverted'
+}
+
+
+const Button = ({ children, buttonType, ...otherProps }: Props) => {
+  return (
+    <button
+      className={`btn-container ${buttonType ? BUTTON_TYPE_CLASSES[buttonType] : ''}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
