3823d213ededbbea52227c712c4e3b1e697b2b97
Fedor Sokolov <sokolov.fedor.g@gmail.com>
2023-08-21 13:10:16 +0400

    Move cartCount state inside `CartIcon` component

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -1,16 +1,23 @@
-import React, { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import './cart-icon.styles.scss'
 import { ReactComponent as ShopingIcon } from 'assets/shopping-bag.svg'
-import { CartContext } from 'contexts/cart.context'
+import { CartContext, CartItemType } from 'contexts/cart.context'
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartItems } = useContext(CartContext);
+  const [cartCount, setCartCount] = useState<number>(0)
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce((total: number, cartItem: CartItemType) => total + cartItem.quantity, 0)
+    setCartCount(newCartCount)
+  }, [cartItems])
+
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div className='cart-icon' onClick={toggleIsCartOpen}>
       <ShopingIcon className='cart-icon__basket' />
-      <span className='cart-icon__count'>0</span>
+      <span className='cart-icon__count'>{cartCount}</span>
     </div>
   )
 }
